cea2e65b1359eca575f160739e2f8d00acb16735
Zodiark92 <91218477+Zodiark92@users.noreply.github.com>
2025-06-30 08:19:04 +0200

    Section 13

---
+++ Section_13_ex_2.sql
@@ -0,0 +1,21 @@
+USE cinema_booking_system;
+
+SELECT * FROM films;
+SELECT * FROM screenings;
+
+-- 1.
+SELECT film_id, start_time FROM screenings
+WHERE date(start_time) = '2022-10-18';
+
+SELECT f.name, s.start_time FROM screenings s
+JOIN films f ON f.id = s.film_id
+WHERE date(start_time) = '2022-10-18';
+
+-- 2.
+SELECT * FROM screenings
+WHERE date(start_time) BETWEEN '2023-03-06' AND '2023-03-13';
+
+-- 3.
+SELECT * FROM screenings
+WHERE month(start_time) = 10
+AND year(start_time) = 2022;
